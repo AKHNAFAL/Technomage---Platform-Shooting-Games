38d7a2cf4d57e2662ada2e26ea295df9f835c1ea
AKHNAFAL <22523179@students.uii.ac.id>
2024-12-19 21:27:32 +0700

    Add PlayerLife to Component Player Inspector

--- Assets/Script/Projectile.cs
+++ Assets/Script/Projectile.cs
@@ -16,6 +16,12 @@ public class Projectile : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        // Abaikan objek dengan tag "OneWayPlatform"
+        if (collision.CompareTag("OneWayPlatform"))
+        {
+            return;
+        }
+
         PlayerController hitPlayer = collision.GetComponent<PlayerController>();
         if (hitPlayer != null)
         {
